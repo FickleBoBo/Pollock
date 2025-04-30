e61eeae3f65c2f2ac02dcad98f6ee8a5dee1d5ad
FickleBoBo <yukmw0704@gmail.com>
2025-05-01 06:41:05 +0900

    feat: 스톡피시 엔진 풀 클래스 작성

---
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/engine/StockfishEnginePool.java
@@ -0,0 +1,44 @@
+package com.pollock.stockfishproxy.engine;
+
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@Slf4j
+@Component
+public class StockfishEnginePool {
+
+    private final BlockingQueue<StockfishEngine> pool;
+
+    public StockfishEnginePool() {
+        this.pool = new LinkedBlockingQueue<>();
+
+        for (int i = 0; i < 4; i++) {
+            StockfishEngine engine = new StockfishEngine();
+
+            if (engine.start()) {
+                pool.add(engine);
+            } else {
+                log.error("❌ StockfishEngine #{} 시작 실패", i);
+            }
+        }
+    }
+
+    public StockfishEngine acquire() throws InterruptedException {
+        return pool.take();
+    }
+
+    public void release(StockfishEngine engine) {
+        pool.add(engine);
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        for (StockfishEngine engine : pool) {
+            engine.quit();
+        }
+    }
+}
