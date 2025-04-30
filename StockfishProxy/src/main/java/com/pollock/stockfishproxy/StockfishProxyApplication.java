34e6ccf5521d919dfa611e28a7ba67022775ef81
FickleBoBo <yukmw0704@gmail.com>
2025-05-01 03:25:56 +0900

    init: 스톡피시 프록시 서버 생성

---
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/StockfishProxyApplication.java
@@ -0,0 +1,13 @@
+package com.pollock.stockfishproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockfishProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockfishProxyApplication.class, args);
+	}
+
+}
---
+++ StockfishProxy/src/test/java/com/pollock/stockfishproxy/StockfishProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollock.stockfishproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfishProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
