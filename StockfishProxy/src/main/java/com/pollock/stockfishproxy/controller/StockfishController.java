42048dacdac07548c376cad732a0ec8a8f520b36
FickleBoBo <yukmw0704@gmail.com>
2025-05-01 06:30:27 +0900

    feat: 스톡피시 엔진 분석 결과 전체 반환 API 작성

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/controller/StockfishController.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/controller/StockfishController.java
@@ -1,11 +1,22 @@
 package com.pollock.stockfishproxy.controller;
 
+import com.pollock.stockfishproxy.service.StockfishService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pollock/stockfish")
 @RequiredArgsConstructor
 public class StockfishController {
+
+    private final StockfishService stockfishService;
+
+    @GetMapping
+    public ResponseEntity<String> getEngineAnalyze(@RequestParam String fen) {
+        return ResponseEntity.ok(stockfishService.getEngineAnalyze(fen));
+    }
 }
