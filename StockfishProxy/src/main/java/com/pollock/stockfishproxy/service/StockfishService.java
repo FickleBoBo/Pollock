bfc747f36d04079f18bab3045c74a778a66b912b
FickleBoBo <yukmw0704@gmail.com>
2025-05-01 06:32:27 +0900

    feat: 스톡피시 프록시 서버 서비스 작성

---
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/service/StockfishService.java
@@ -0,0 +1,9 @@
+package com.pollock.stockfishproxy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StockfishService {
+}
