af331a435378d99c489b211b6a44e0fadb1b0881
FickleBoBo <yukmw0704@gmail.com>
2025-05-01 06:28:57 +0900

    feat: 스톡피시 프록시 서버 컨트롤러 작성

---
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/controller/StockfishController.java
@@ -0,0 +1,11 @@
+package com.pollock.stockfishproxy.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pollock/stockfish")
+@RequiredArgsConstructor
+public class StockfishController {
+}
