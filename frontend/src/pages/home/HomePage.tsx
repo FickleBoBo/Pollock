8365bb2eefa3ff4f901202b55f18af01207edfe1
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 13:14:56 +0900

    feat: 분석 페이지 라우팅 추가

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 import Header from "../../components/common/Header.tsx";
 import Footer from "../../components/common/Footer.tsx";
+import Button from "../../components/common/Button.tsx";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div>
         <Header />
+      </div>
+      <div>
+        <Button text="분석하기" onClick={() => navigate("/analysis")} />
       </div>
       <div>
         <Footer />
