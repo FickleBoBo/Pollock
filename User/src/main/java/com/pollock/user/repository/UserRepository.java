af02fef5560b0e38ffd18caf841f1a294ac6ae6f
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 09:46:43 +0900

    feat: 옵셔널 처리

--- User/src/main/java/com/pollock/user/repository/UserRepository.java
+++ User/src/main/java/com/pollock/user/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.pollock.user.repository;
 import com.pollock.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    UserEntity findByEmail(String email);
+    Optional<UserEntity> findByEmail(String email);
 }
