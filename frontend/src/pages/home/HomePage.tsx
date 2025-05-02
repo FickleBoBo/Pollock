19d0ff84fcafaed7df68633dee1b8299563ba7e3
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 13:08:55 +0900

    feat: 랜딩 페이지 작성

---
+++ frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,17 @@
+import Header from "../../components/common/Header.tsx";
+import Footer from "../../components/common/Footer.tsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
