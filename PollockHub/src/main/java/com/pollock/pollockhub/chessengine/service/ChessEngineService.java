e758a203a1a1ee6a42eb98ea618c2069f27cb981
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 10:50:59 +0900

    feat: 체스 엔진 서비스 작성

--- PollockHub/src/main/java/com/pollock/pollockhub/chessengine/service/ChessEngineService.java
+++ PollockHub/src/main/java/com/pollock/pollockhub/chessengine/service/ChessEngineService.java
@@ -1,9 +1,13 @@
 package com.pollock.pollockhub.chessengine.service;
 
+import com.pollock.pollockhub.chessengine.dto.response.EngineAnalysisResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ChessEngineService {
+
+    public EngineAnalysisResponseDTO getEngineAnalysis() {
+    }
 }
