c0c375a846ae1ad0c6ec6a422d17054119974761
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 16:40:38 +0900

    feat: 퍼블리셔 컴포넌트 작성

---
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/redis/RedisPublisher.java
@@ -0,0 +1,16 @@
+package com.pollock.stockfishproxy.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisPublisher {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void publish(String channel, String message) {
+        stringRedisTemplate.convertAndSend(channel, message);
+    }
+}
