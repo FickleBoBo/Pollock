20b6995a480ec86623ad713c7136ace896ba6c97
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 10:51:07 +0900

    feat: 체스 엔진 컨트롤러 작성

--- PollockHub/src/main/java/com/pollock/pollockhub/chessengine/controller/ChessEngineController.java
+++ PollockHub/src/main/java/com/pollock/pollockhub/chessengine/controller/ChessEngineController.java
@@ -1,7 +1,11 @@
 package com.pollock.pollockhub.chessengine.controller;
 
+import com.pollock.pollockhub.chessengine.dto.response.EngineAnalysisResponseDTO;
+import com.pollock.pollockhub.chessengine.service.ChessEngineService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequiredArgsConstructor
 public class ChessEngineController {
+
+    private final ChessEngineService chessEngineService;
+
+    @GetMapping
+    public ResponseEntity<EngineAnalysisResponseDTO> getEngineAnalysis() {
+        return ResponseEntity.ok(chessEngineService.getEngineAnalysis());
+    }
 }
