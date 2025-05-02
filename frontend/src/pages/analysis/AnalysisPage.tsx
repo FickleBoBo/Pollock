e16e50656ca1aa399c666a2c26bd28f382681f3e
FickleBoBo <yukmw0704@gmail.com>
2025-05-02 13:15:28 +0900

    style: 분석 페이지 이름 변경

---
+++ frontend/src/pages/analysis/AnalysisPage.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Chess } from "chess.js";
+import ChessBoard from "../../components/ChessBoard";
+import ChessEngine from "../../components/ChessEngine";
+
+const AnalysisPage = () => {
+  const [game, setGame] = useState(new Chess());
+  const [analysis, setAnalysis] = useState("");
+
+  return (
+    <div>
+      <ChessBoard game={game} setGame={setGame} />
+      <ChessEngine
+        fen={game.fen()}
+        analysis={analysis}
+        setAnalysis={setAnalysis}
+      />
+      김치
+    </div>
+  );
+};
+
+export default AnalysisPage;
