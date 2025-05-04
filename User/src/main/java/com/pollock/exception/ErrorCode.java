3f3c02c7c0203fd4cb1cb28e713dcfef3954baf5
FickleBoBo <yukmw0704@gmail.com>
2025-05-05 08:31:01 +0900

    feat: 에러 유형 enum 작성

---
+++ User/src/main/java/com/pollock/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.pollock.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    UnAuthenticatedException(401, "UNAUTHORIZED", "로그인하지 않은 사용자입니다."),
+    InternalServerErrorException(500, "INTERNAL_SERVER_ERROR", "내부 서버 에러");
+
+    private final int httpStatus;
+    private final String code;
+    private final String message;
+}
