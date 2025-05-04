a764773df55e8ec65cc3435fc4a3db4932e39876
FickleBoBo <yukmw0704@gmail.com>
2025-05-05 08:31:46 +0900

    feat: CORS 어노테이션 제거

--- User/src/main/java/com/pollock/user/controller/UserController.java
+++ User/src/main/java/com/pollock/user/controller/UserController.java
@@ -6,14 +6,12 @@ import com.pollock.user.oauth2.dto.CustomOAuth2User;
 import com.pollock.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pollock/user")
-@CrossOrigin
 @RequiredArgsConstructor
 public class UserController {
 
