70b734deef5d6f9bdcff0c429141b4cf179970f1
FickleBoBo <yukmw0704@gmail.com>
2025-05-06 08:30:28 +0900

    feat: 분석 중단 로직 추가 및 로깅

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/service/StockfishService.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/service/StockfishService.java
@@ -8,29 +8,73 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class StockfishService {
 
     private final StockfishEnginePool pool;
+    private final Map<Long, Thread> threadMap = new ConcurrentHashMap<>();
     private final RedisPublisher redisPublisher;
 
     public void publishEngineAnalysis(EngineAnalysisRequestDTO requestDTO) {
-        new Thread(() -> {
+        Thread thread = new Thread(() -> {
             StockfishEngine engine = null;
+
             try {
+                log.info("엔진 얻기 전");
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
                 engine = pool.acquire();
+                log.info("엔진 얻은 후");
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
 
                 log.info("Engine PID: {}", engine.getEnginePid());
+
+                engine.publishEngineAnalysis(requestDTO.getGameId(), requestDTO.getFen(), requestDTO.getMultiPV(), requestDTO.getMoveTime(), redisPublisher);
+
             } catch (InterruptedException e) {
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
                 log.error("스톡피시 엔진 풀에서 엔진 얻기 실패: {}", e.getMessage(), e);
-                throw new RuntimeException(e);
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+
+                Thread.currentThread().interrupt();
+            } finally {
+                if (engine != null) {
+                    log.info("finally 블록 엔진 반환 전");
+                    log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+                    pool.release(engine);
+                    log.info("finally 블록 엔진 반환 후");
+                    log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+                }
+                log.info("finally 블록2 엔진 반환 전");
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+                threadMap.remove(requestDTO.getGameId());
+                log.info("finally 블록2 엔진 반환 후");
+                log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
             }
+        });
 
-            engine.publishEngineAnalysis(requestDTO.getGameId(), requestDTO.getFen(), requestDTO.getMultiPV(), requestDTO.getMoveTime(), redisPublisher);
+        log.info("map에 넣기 전");
+        log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+        threadMap.put(requestDTO.getGameId(), thread);
+        log.info("map에 넣은 후");
+        log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+        thread.start();
+    }
 
-            pool.release(engine);
-        }).start();
+    public void cancelEngineAnalysis(Long gameId) {
+        Thread thread = threadMap.get(gameId);
+
+        if (thread != null && thread.isAlive()) {
+            log.info("🛑 분석 중단 요청: gameId = {}", gameId);
+            log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+            thread.interrupt(); // 스레드 인터럽트 요청
+            log.info("pool 크기: {}, map 크기: {}", pool.getPoolSize(), threadMap.size());
+        } else {
+            log.warn("⚠️ 중단 요청 실패: 실행 중인 분석이 없음 (gameId = {})", gameId);
+        }
     }
 }
