4050b95279019ff23f3bfc0556408ea9e5603ac3
FickleBoBo <yukmw0704@gmail.com>
2025-05-06 20:38:59 +0900

    init: user, pollock-hub 통합 프로젝트 생성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/PollockHubApplication.java
@@ -0,0 +1,13 @@
+package com.pollock.pollockhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollockHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollockHubApplication.class, args);
+    }
+
+}
