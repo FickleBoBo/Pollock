4ad6722c8fa68c47d2232d7176b5c5538ecf6a6b
FickleBoBo <yukmw0704@gmail.com>
2025-05-06 20:53:50 +0900

    style: 프리티어 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "pollock.kr", // 로컬 도메인 (hosts에 등록한 것)
-    port: 5173, // Vite 기본 포트
-    cors: true, // 개발용 CORS 허용
-    origin: "http://pollock.kr:5173", // 명시적으로 origin 지정
+  define: {
+    global: {},
   },
 });
