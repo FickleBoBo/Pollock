f29a4dc35002383bbe26ba706c412367e7d9746e
FickleBoBo <yukmw0704@gmail.com>
2025-05-08 10:41:25 +0900

    feat: 엔진 서비스 라우터 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/router/EngineServiceRouter.java
@@ -0,0 +1,27 @@
+package com.pollock.pollockhub.engine.router;
+
+import com.pollock.pollockhub.engine.exception.EngineNotFoundException;
+import com.pollock.pollockhub.engine.service.EngineService;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class EngineServiceRouter {
+
+    private final Map<EngineType, EngineService> engineServiceMap;
+
+    public EngineServiceRouter(List<EngineService> engineServices) {
+        this.engineServiceMap = new HashMap<>();
+        for (EngineService engineService : engineServices) {
+            engineServiceMap.put(engineService.getEngineType(), engineService);
+        }
+    }
+
+    public EngineService resolve(EngineType engineType) {
+        return Optional.ofNullable(engineServiceMap.get(engineType)).orElseThrow(EngineNotFoundException::getInstance);
+    }
+}
