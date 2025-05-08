c99fdf028fea3fdacce4697e85f6740c7bf54b0b
FickleBoBo <yukmw0704@gmail.com>
2025-05-08 12:59:13 +0900

    feat: 엔진 타입 json 매핑 추가

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
@@ -1,12 +1,15 @@
 package com.pollock.stockfishproxy.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pollock.stockfishproxy.router.StockfishEngineType;
 import lombok.Getter;
 
 @Getter
 public class EngineAnalysisRequestDTO {
 
+    @JsonProperty("engineType")
     private StockfishEngineType stockfishEngineType;
+
     private String fen;
     private Integer multiPV;
     private Long moveTime;
