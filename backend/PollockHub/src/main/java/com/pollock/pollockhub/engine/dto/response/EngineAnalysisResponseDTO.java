8d9334baa9c2c9b56a1317d333e788e4b1ce0704
FickleBoBo <yukmw0704@gmail.com>
2025-05-08 10:37:08 +0900

    feat: 엔진 분석 응답 DTO 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/dto/response/EngineAnalysisResponseDTO.java
@@ -0,0 +1,11 @@
+package com.pollock.pollockhub.engine.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class EngineAnalysisResponseDTO {
+
+    private String streamKey;
+}
