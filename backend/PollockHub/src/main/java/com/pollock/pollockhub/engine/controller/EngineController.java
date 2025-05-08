d6e8f803909b27fe7b4cb9b504267b4fea4bd5db
FickleBoBo <yukmw0704@gmail.com>
2025-05-08 12:07:05 +0900

    feat: 응답 dto 변경

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/controller/EngineController.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/controller/EngineController.java
@@ -2,6 +2,7 @@ package com.pollock.pollockhub.engine.controller;
 
 import com.pollock.pollockhub.engine.dto.request.EngineAnalysisRequestDTO;
 import com.pollock.pollockhub.engine.dto.response.EngineAnalysisResponseDTO;
+import com.pollock.pollockhub.engine.dto.response.RedisStreamKeyResponseDTO;
 import com.pollock.pollockhub.engine.router.EngineServiceRouter;
 import com.pollock.pollockhub.engine.service.EngineService;
 import com.pollock.pollockhub.engine.service.RedisStreamService;
@@ -18,14 +19,14 @@ public class EngineController {
     private final RedisStreamService redisStreamService;
 
     @PostMapping
-    ResponseEntity<EngineAnalysisResponseDTO> startEngineAnalysis(@RequestBody EngineAnalysisRequestDTO requestDTO) {
+    ResponseEntity<RedisStreamKeyResponseDTO> startEngineAnalysis(@RequestBody EngineAnalysisRequestDTO requestDTO) {
         EngineService engineService = engineServiceRouter.resolve(requestDTO.getEngineType());
         return ResponseEntity.ok(engineService.startEngineAnalysis(requestDTO));
     }
 
     @GetMapping
-    ResponseEntity<String> getEngineAnalysis(@RequestParam String streamKey,
-                                             @RequestParam(defaultValue = "0-0") String lastId) {
+    ResponseEntity<EngineAnalysisResponseDTO> getEngineAnalysis(@RequestParam String streamKey,
+                                                                @RequestParam(defaultValue = "0-0") String lastId) {
         return ResponseEntity.ok(redisStreamService.getEngineAnalysis(streamKey, lastId));
     }
 }
