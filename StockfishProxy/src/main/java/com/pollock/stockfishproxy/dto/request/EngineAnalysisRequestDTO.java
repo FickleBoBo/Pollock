a36f9bc1c749707a565c105505e818464afb25e4
FickleBoBo <yukmw0704@gmail.com>
2025-05-08 13:12:54 +0900

    feat: 엔진 타입 필드 제거

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
@@ -1,14 +1,9 @@
 package com.pollock.stockfishproxy.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.pollock.stockfishproxy.router.StockfishType;
 import lombok.Getter;
 
 @Getter
 public class EngineAnalysisRequestDTO {
-
-    @JsonProperty("engineType")
-    private StockfishType stockfishType;
 
     private String fen;
     private Integer multiPV;
