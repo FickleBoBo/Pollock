d87524f399ed9a349c542189b0ddbbc95f141320
FickleBoBo <yukmw0704@gmail.com>
2025-05-16 05:18:55 +0900

    feat: 로그인 페이지 작성

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,12 +1,20 @@
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
 import NaverLoginButton from "../../components/login/NaverLoginButton";
 
 const LoginPage = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <div>
+        <Header />
+      </div>
+      <div className="flex flex-grow justify-center items-center">
         <NaverLoginButton />
       </div>
-    </>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 };
 
