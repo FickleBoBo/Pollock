d4efc24fe0fbb97c4d329ce83a1baf9d3e292370
FickleBoBo <yukmw0704@gmail.com>
2025-05-16 05:17:31 +0900

    feat: 헤더 작성

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { BiLogIn, BiLogOut } from "react-icons/bi";
+
 import api from "../../common/api";
+import axios from "axios";
 
 import Button from "./Button";
 
@@ -16,55 +19,77 @@ const Header = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const navigate = useNavigate();
 
+  // 유저 정보 요청
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
         const { data } = await api.get<UserInfo>("/api/pollock/user/me");
         setUserInfo(data);
-      } catch (error) {
-        console.error("유저 정보 요청 실패:", error);
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          const status = error.response?.status;
+
+          if (status !== 401) {
+            console.error("유저 정보 요청 실패:", error);
+          }
+        } else {
+          console.error("알 수 없는 에러:", error);
+        }
       }
     };
 
     fetchUserInfo();
   }, []);
 
+  // 로그아웃 핸들러
   const handleLogout = async () => {
     try {
       await api.post("/api/pollock/user/logout");
-      navigate("/");
+      window.location.href = "/";
     } catch (error) {
       console.error("로그아웃 실패:", error);
     }
   };
 
   return (
-    <div className="flex justify-between bg-red-600">
-      <div>
-        <Button text="Pollock" onClick={() => window.location.reload()} />
-      </div>
-      <div>
-        {userInfo ? (
-          <>
-            <div className="flex">
+    <header>
+      <div className="flex justify-between bg-gray-700 px-4 py-8">
+        <div>
+          <Button
+            className="text-xl font-bold"
+            text="Pollock"
+            onClick={() => (window.location.href = "/")}
+          />
+        </div>
+        <div>
+          {userInfo ? (
+            <div className="flex items-center gap-4">
               <div>
-                <img src={userInfo.profileImageUrl} alt="profile" />
+                <img
+                  src={userInfo.profileImageUrl}
+                  alt="profile"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
               </div>
               <div>{userInfo.nickname}</div>
-              <div>{userInfo.grade}</div>
-            </div>
-            <div>
               <div>ELO {userInfo.elo}</div>
-              <Button text="logout" onClick={handleLogout} />
+              <div>{userInfo.grade}</div>
+              <div className="flex">
+                <Button onClick={handleLogout}>
+                  <BiLogOut size={32} />
+                </Button>
+              </div>
             </div>
-          </>
-        ) : (
-          <div>
-            <Button text="login" onClick={() => navigate("/login")} />
-          </div>
-        )}
+          ) : (
+            <div className="flex">
+              <Button onClick={() => navigate("/login")}>
+                <BiLogIn size={32} />
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
