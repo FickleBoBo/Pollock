b9f748adfa143ab20c498f844e8ab95ceffc7aa7
FickleBoBo <yukmw0704@gmail.com>
2025-05-16 05:20:02 +0900

    feat: 네이버 로그인 버튼 작성

--- frontend/src/components/login/NaverLoginButton.tsx
+++ frontend/src/components/login/NaverLoginButton.tsx
@@ -1,10 +1,10 @@
 const NaverLoginButton = () => {
   return (
-    <div>
+    <div className="my-4">
       <a
         // href="http://www.pollock.kr/api/pollock/user/oauth2/authorization/naver"
         href="http://localhost:8080/api/pollock/user/oauth2/authorization/naver"
-        className="bg-green-600"
+        className="bg-[#03C75A] hover:bg-[#029E4F] text-xl font-bold px-24 py-4 rounded"
       >
         Naver 로그인
       </a>
