e4cb488fa8eca1a0e58c697b977d25cd9883e1ab
FickleBoBo <yukmw0704@gmail.com>
2025-05-17 07:00:29 +0900

    style: css 수정

--- frontend/src/components/login/NaverLoginButton.tsx
+++ frontend/src/components/login/NaverLoginButton.tsx
@@ -1,10 +1,10 @@
 const NaverLoginButton = () => {
   return (
-    <div className="my-4">
+    <div className="my-8">
       <a
         // href="http://www.pollock.kr/api/pollock/user/oauth2/authorization/naver"
         href="http://localhost:8080/api/pollock/user/oauth2/authorization/naver"
-        className="bg-[#03C75A] hover:bg-[#029E4F] text-xl font-bold px-24 py-4 rounded"
+        className="bg-[#03C75A] hover:bg-[#029E4F] text-xl font-bold px-32 py-8 rounded"
       >
         Naver 로그인
       </a>
