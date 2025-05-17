606ac30099167891979355c0d229efbb89de1ca3
FickleBoBo <yukmw0704@gmail.com>
2025-05-17 19:20:08 +0900

    feat: 학습 탭 드롭 다운으로 변경

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -3,17 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 import { BiLogIn, BiLogOut } from "react-icons/bi";
 
+import { UserInfo } from "../../constant/User";
+
 import axios from "axios";
 import api from "../../common/api";
 
 import Button from "./Button";
-
-interface UserInfo {
-  nickname: string;
-  profileImageUrl: string;
-  elo: number;
-  grade: string;
-}
 
 const Header = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
@@ -54,8 +49,10 @@ const Header = () => {
 
   return (
     <header className="mb-16">
-      <div className="flex justify-between p-8 bg-gray-700">
-        <div className="flex items-center gap-16">
+      <div className="flex justify-between items-center px-16 bg-gray-700">
+        {/* 헤더 좌측 */}
+        <div className="flex items-center">
+          {/* 메인 로고 */}
           <div className="mr-32">
             <Button
               className="text-3xl font-bold"
@@ -63,21 +60,37 @@ const Header = () => {
               onClick={() => (window.location.href = "/")}
             />
           </div>
-          <div>
-            <Button
-              className="text-xl font-bold"
-              text="학습"
-              onClick={() => navigate("/learn")}
-            />
+          {/* 학습 */}
+          <div className="px-16 py-8 hover:bg-gray-600 transition relative group">
+            <div className="text-xl font-bold">학습</div>
+
+            <div className="absolute top-full left-0 w-full hidden group-hover:block bg-gray-700">
+              <div className="hover:bg-gray-600 transition">
+                <Button
+                  className="w-full py-4 text-xl font-bold"
+                  text="기보 학습"
+                  onClick={() => navigate("/learn/notation")}
+                />
+              </div>
+              <div className="hover:bg-gray-600 transition">
+                <Button
+                  className="w-full py-4 text-xl font-bold"
+                  text="전술 학습"
+                  onClick={() => navigate("/learn/tactics")}
+                />
+              </div>
+            </div>
           </div>
-          <div>
+          {/* 분석 */}
+          <div className="hover:bg-gray-600 transition">
             <Button
-              className="text-xl font-bold"
+              className="text-xl font-bold px-16 py-8"
               text="분석"
               onClick={() => navigate("/analysis")}
             />
           </div>
         </div>
+        {/* 헤더 우측 */}
         <div>
           {userInfo ? (
             <div className="flex items-center gap-4">
