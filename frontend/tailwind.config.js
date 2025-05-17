76aea6ceafcbf509b33515ab7c98e92d6077a50e
FickleBoBo <yukmw0704@gmail.com>
2025-05-17 21:42:48 +0900

    feat: 배경색 커스텀 스타일 적용

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayLightest: "#F3F4F6",
+        grayLight: "#9CA3AF",
+        grayBase: "#4B5563",
+        grayDark: "#374151",
+        grayDarkest: "#1F2937",
+      },
+    },
   },
   plugins: [],
 };
