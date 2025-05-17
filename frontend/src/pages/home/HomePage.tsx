75e5ea534f0ac7636098f911443db51c8b16f619
FickleBoBo <yukmw0704@gmail.com>
2025-05-17 22:00:29 +0900

    style: 커스텀 색상 적용

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,8 +4,6 @@ import { FaRobot, FaUserFriends } from "react-icons/fa";
 
 import { gameModes } from "../../constant/GameModes";
 
-import api from "../../common/api";
-
 import Header from "../../components/common/Header";
 import Footer from "../../components/common/Footer";
 import Button from "../../components/common/Button";
@@ -20,17 +18,7 @@ const HomePage = () => {
 
   // 새 게임 시작 핸들러
   const handleStartNewGame = async (gameType: number) => {
-    try {
-      const response = await api.post("/api/pollock/game/new-game", {
-        gameType,
-      });
-      const { roomId } = response.data;
-
-      navigate(`/play/${roomId}`);
-    } catch (error) {
-      console.error("게임 시작 실패:", error);
-      alert("게임을 시작할 수 없습니다.");
-    }
+    alert(`아직 ${gameType} 게임을 시작할 수 없습니다.`);
   };
 
   return (
@@ -49,7 +37,7 @@ const HomePage = () => {
             <Button
               text="새 게임 생성"
               onClick={handleCreateNewGame}
-              className="w-full text-xl font-bold p-4 bg-gray-700 hover:bg-gray-600"
+              className="w-full text-xl font-bold p-4 bg-grayDark hover:bg-grayBase"
             />
           </div>
 
@@ -58,7 +46,7 @@ const HomePage = () => {
               <Button
                 key={index}
                 onClick={() => handleStartNewGame(mode.gameType)}
-                className="w-full flex flex-col justify-center gap-8 text-3xl font-bold p-4 bg-gray-700 hover:bg-gray-600"
+                className="w-full flex flex-col justify-center gap-8 text-3xl font-bold p-4 bg-grayDark hover:bg-grayBase"
               >
                 <div>{mode.timeControl}</div>
                 <div>{mode.gameFormat}</div>
@@ -70,7 +58,7 @@ const HomePage = () => {
             <div className="flex-1">
               <Button
                 onClick={() => navigate("/play/computer")}
-                className="w-full font-bold p-4 bg-gray-700 hover:bg-gray-600"
+                className="w-full font-bold p-4 bg-grayDark hover:bg-grayBase"
               >
                 <div className="flex justify-center items-center gap-4">
                   <FaRobot size={32} />
@@ -81,7 +69,7 @@ const HomePage = () => {
             <div className="flex-1">
               <Button
                 onClick={() => navigate("/play/friend")}
-                className="w-full font-bold p-4 bg-gray-700 hover:bg-gray-600"
+                className="w-full font-bold p-4 bg-grayDark hover:bg-grayBase"
               >
                 <div className="flex justify-center items-center gap-4">
                   <FaUserFriends size={32} />
