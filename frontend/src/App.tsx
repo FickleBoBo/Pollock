20f1f850f95214cb52b6bb8886c6a11bcbb11aac
FickleBoBo <yukmw0704@gmail.com>
2025-05-17 10:04:04 +0900

    feat: 설정 페이지 라우팅 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/login/LoginPage";
+import SettingPage from "./pages/setting/SettingPage";
 import LearnPage from "./pages/learn/LearnPage";
 import AnalysisPage from "./pages/analysis/AnalysisPage";
 import ComputerPage from "./pages/play/computer/ComputerPage";
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingPage />} />
         <Route path="/learn" element={<LearnPage />} />
         <Route path="/analysis" element={<AnalysisPage />} />
         <Route path="/play/computer" element={<ComputerPage />} />
