f5bf3f1859ad5401d1e9aba5a255168493c3c147
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 07:38:55 +0900

    feat: 채널 필드 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/dto/request/EngineAnalysisRequestDTO.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/dto/request/EngineAnalysisRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 public class EngineAnalysisRequestDTO {
 
+    private String channelKey;
     private EngineType engineType;
     private String fen;
     private Integer multiPV;
