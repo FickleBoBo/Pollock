c4ef28470fe632cd9423622dd970889a2cc5f1c2
FickleBoBo <yukmw0704@gmail.com>
2025-05-18 21:21:53 +0900

    feat: 게임 예외 에러코드 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/common/ErrorCode.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/common/ErrorCode.java
@@ -11,6 +11,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
     UnAuthenticatedException(UNAUTHORIZED, 401, "로그인하지 않은 사용자입니다."),
     UserNotFoundException(NOT_FOUND, 404, "해당 유저를 찾을 수 없습니다."),
+    GameNotFoundException(NOT_FOUND, 404, "해당 게임을 찾을 수 없습니다."),
     EngineNotFoundException(NOT_FOUND, 404, "해당 엔진을 찾을 수 없습니다."),
 
     InternalServerErrorException(INTERNAL_SERVER_ERROR, 500, "내부 서버 에러"),
