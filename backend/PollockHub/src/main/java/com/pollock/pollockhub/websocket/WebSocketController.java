53b8468064c4aa9655d9ebd3e2de91b01f4a0a54
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 08:00:46 +0900

    feat: 웹소켓 연결 확인용 컨트롤러

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/websocket/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.pollock.pollockhub.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class WebSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/heartbeat")
+    public void heartbeat(String message) {
+        messagingTemplate.convertAndSend("/topic/ping", "pong");
+    }
+}
