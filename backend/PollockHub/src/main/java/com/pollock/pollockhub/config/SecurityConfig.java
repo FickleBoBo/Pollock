8d3ec9483ae702de31d134088ada3c064ca73a1a
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 08:32:34 +0900

    feat: 웹소켓 경로 허용 설정

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/config/SecurityConfig.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -77,5 +78,10 @@ public class SecurityConfig {
                 .authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ws/**");
     }
 }
