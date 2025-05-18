3a684af8d7b6360cb36addf288eecf707fcdea2a
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 07:35:47 +0900

    feat: 게임 결과 enum 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/game/entity/GameResult.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/game/entity/GameResult.java
@@ -1,5 +1,13 @@
 package com.pollock.pollockhub.game.entity;
 
 public enum GameResult {
-    BLACK_WIN, WHITE_WIN, DRAW
+    BLACK_WIN,
+    WHITE_WIN,
+    DRAW,
+    WHITE_TIMEOUT,
+    BLACK_TIMEOUT,
+    WHITE_RESIGN,
+    BLACK_RESIGN,
+    STALEMATE,
+    ABANDONED
 }
