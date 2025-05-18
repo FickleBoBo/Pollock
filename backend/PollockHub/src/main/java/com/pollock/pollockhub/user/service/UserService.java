c6be73e08a78ef57ad0436292eeac0b528d937b0
FickleBoBo <yukmw0704@gmail.com>
2025-05-18 21:04:49 +0900

    feat: 게임 아이디로 유저 조회 서비스 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/service/UserService.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/service/UserService.java
@@ -1,5 +1,10 @@
 package com.pollock.pollockhub.user.service;
 
+import com.pollock.pollockhub.game.entity.GameEntity;
+import com.pollock.pollockhub.game.exception.GameNotFoundException;
+import com.pollock.pollockhub.game.repository.GameRepository;
+import com.pollock.pollockhub.user.dto.response.UserInfo;
+import com.pollock.pollockhub.user.dto.response.UserInfoListResponseDTO;
 import com.pollock.pollockhub.user.dto.response.UserInfoResponseDTO;
 import com.pollock.pollockhub.user.entity.UserEntity;
 import com.pollock.pollockhub.user.exception.UserNotFoundException;
@@ -8,11 +13,14 @@ import com.pollock.pollockhub.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final GameRepository gameRepository;
 
     public UserInfoResponseDTO getUserInfo(CustomOAuth2User user) {
         UserEntity userEntity = userRepository.findById(user.getId()).orElseThrow(UserNotFoundException::getInstance);
@@ -25,6 +33,16 @@ public class UserService {
                 .birthyear(userEntity.getBirthyear())
                 .gender(userEntity.getGender())
                 .grade(userEntity.getGrade())
+                .build();
+    }
+
+    public UserInfoListResponseDTO getUserInfoByGameId(Long gameId) {
+        GameEntity gameEntity = gameRepository.findById(gameId).orElseThrow(GameNotFoundException::getInstance);
+
+        return UserInfoListResponseDTO.builder()
+                .users(Map.of(
+                        "black", UserInfo.from(gameEntity.getBlackUser()),
+                        "white", UserInfo.from(gameEntity.getWhiteUser())))
                 .build();
     }
 }
