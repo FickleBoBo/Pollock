81eaa092e15790e3f9c75eb6cba53bb2f9bfdd0e
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 23:25:12 +0900

    feat: 엔진 버전 필드 무시

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/request/EngineAnalysisRequestDTO.java
@@ -1,10 +1,13 @@
 package com.pollock.stockfishproxy.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EngineAnalysisRequestDTO {
 
+    private String channelKey;
     private String fen;
     private Integer multiPV;
     private Long moveTime;
