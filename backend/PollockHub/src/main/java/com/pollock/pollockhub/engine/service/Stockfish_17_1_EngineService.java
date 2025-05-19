e3f2ff2b25fa847abcdd2b9e4560e8131bb6472e
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 23:05:15 +0900

    feat: 스톡피시 엔진 서비스 파일명 및 경로 변경

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/service/Stockfish_17_1_EngineService.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/service/Stockfish_17_1_EngineService.java
@@ -4,24 +4,21 @@ import com.pollock.pollockhub.common.exception.BadGatewayException;
 import com.pollock.pollockhub.engine.dto.request.EngineAnalysisRequestDTO;
 import com.pollock.pollockhub.engine.router.EngineType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import static com.pollock.pollockhub.engine.router.EngineType.STOCKFISH_17_1;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
-public class Stockfish17EngineService implements EngineService {
+public class Stockfish_17_1_EngineService implements EngineService {
 
     private final RestTemplate restTemplate;
 
     @Value("${custom.stockfish-17-1-proxy-server-uri}")
-    private String stockfish17ProxyServerUri;
+    private String stockfish_17_1_ProxyServerUri;
 
     @Override
     public EngineType getEngineType() {
@@ -31,13 +28,8 @@ public class Stockfish17EngineService implements EngineService {
     @Override
     public void getEngineAnalysis(EngineAnalysisRequestDTO requestDTO) {
         try {
-            ResponseEntity<Void> responseEntity = restTemplate.postForEntity(stockfish17ProxyServerUri, requestDTO, Void.class);
-
-            if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-                log.warn("❗️Stockfish17 비정상 응답: {}", responseEntity.getStatusCode());
-            }
+            restTemplate.postForEntity(stockfish_17_1_ProxyServerUri, requestDTO, Void.class);
         } catch (RestClientException e) {
-            log.error("❌ Stockfish17 분석 요청 실패: {}", e.getMessage(), e);
             throw BadGatewayException.getInstance();
         }
     }
