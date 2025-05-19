26d68f6dc75def6613723857bedbf7e0d4279031
FickleBoBo <yukmw0704@gmail.com>
2025-05-19 23:25:59 +0900

    feat: 엔진 응답 dto 작성

--- StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/response/EngineAnalysisResponseDTO.java
+++ StockfishProxy/src/main/java/com/pollock/stockfishproxy/dto/response/EngineAnalysisResponseDTO.java
@@ -1,4 +1,16 @@
 package com.pollock.stockfishproxy.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
 public class EngineAnalysisResponseDTO {
+
+    private Integer score;
+    private Integer mate;
+    private Integer currentPv;
+    private List<String> pvList;
 }
