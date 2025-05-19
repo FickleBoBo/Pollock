606317804fedb911272dda87a774e7669a0ade3e
FickleBoBo <yukmw0704@gmail.com>
2025-05-20 02:35:20 +0900

    style: 폼 css 수정

--- frontend/src/components/analysis/EngineSettingForm.tsx
+++ frontend/src/components/analysis/EngineSettingForm.tsx
@@ -1,47 +1,53 @@
-interface EngineSettingsFormProps {
+import {
+  engineTypeOption,
+  multipvOption,
+  movetimeOption,
+} from "../../constant/Engine";
+
+interface EngineSettingFormProps {
   engineType: string;
   setEngineType: (type: string) => void;
-  multiPV: number;
-  setMultiPV: (value: number) => void;
-  moveTime: number;
-  setMoveTime: (value: number) => void;
+  multipv: number;
+  setMultipv: (value: number) => void;
+  movetime: number;
+  setMovetime: (value: number) => void;
 }
 
-const EngineSettingsForm = ({
+const EngineSettingForm = ({
   engineType,
   setEngineType,
-  multiPV,
-  setMultiPV,
-  moveTime,
-  setMoveTime,
-}: EngineSettingsFormProps) => {
+  multipv,
+  setMultipv,
+  movetime,
+  setMovetime,
+}: EngineSettingFormProps) => {
   return (
-    <div className="w-full flex flex-col items-center gap-8">
+    <div className="flex flex-col gap-8 p-4 bg-grayDark">
       {/* 체스 엔진 선택 */}
-      <div className="w-full">
-        <label className="block mb-4">체스 엔진</label>
+      <div className="flex flex-col gap-4">
+        <label>체스 엔진</label>
         <select
           value={engineType}
           onChange={(e) => setEngineType(e.target.value)}
-          className="w-full block text-black px-2 py-1 rounded"
+          className="text-black p-2 rounded"
         >
-          {["STOCKFISH_17"].map((v) => (
-            <option key={v} value={v}>
-              {v}
+          {Object.entries(engineTypeOption).map(([label, value]) => (
+            <option key={value} value={value}>
+              {label}
             </option>
           ))}
         </select>
       </div>
 
       {/* 라인 수 선택 */}
-      <div className="w-full">
-        <label className="block mb-4">라인 수</label>
+      <div className="flex flex-col gap-4">
+        <label>라인 수</label>
         <select
-          value={multiPV}
-          onChange={(e) => setMultiPV(Number(e.target.value))}
-          className="w-full block text-black px-2 py-1 rounded"
+          value={multipv}
+          onChange={(e) => setMultipv(Number(e.target.value))}
+          className="text-black p-2 rounded"
         >
-          {[1, 2, 3, 4, 5].map((v) => (
+          {multipvOption.map((v) => (
             <option key={v} value={v}>
               {v}
             </option>
@@ -50,14 +56,14 @@ const EngineSettingsForm = ({
       </div>
 
       {/* 최대 시간 선택 */}
-      <div className="w-full">
-        <label className="block mb-4">최대 시간</label>
+      <div className="flex flex-col gap-4">
+        <label>최대 시간</label>
         <select
-          value={moveTime}
-          onChange={(e) => setMoveTime(Number(e.target.value))}
-          className="w-full block text-black px-2 py-1 rounded"
+          value={movetime}
+          onChange={(e) => setMovetime(Number(e.target.value))}
+          className="text-black p-2 rounded"
         >
-          {[3000, 5000, 10000, 20000, 30000, 60000].map((ms) => (
+          {movetimeOption.map((ms) => (
             <option key={ms} value={ms}>
               {ms / 1000}초
             </option>
@@ -68,4 +74,4 @@ const EngineSettingsForm = ({
   );
 };
 
-export default EngineSettingsForm;
+export default EngineSettingForm;
