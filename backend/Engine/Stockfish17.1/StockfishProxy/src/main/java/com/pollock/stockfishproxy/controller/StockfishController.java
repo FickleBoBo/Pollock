8971785aaf52f529c30a010e45538e66c7437ae1
FickleBoBo <yukmw0704@gmail.com>
2025-05-20 10:58:11 +0900

    feat: 로그 제거

--- backend/Engine/Stockfish17.1/StockfishProxy/src/main/java/com/pollock/stockfishproxy/controller/StockfishController.java
+++ backend/Engine/Stockfish17.1/StockfishProxy/src/main/java/com/pollock/stockfishproxy/controller/StockfishController.java
@@ -3,14 +3,12 @@ package com.pollock.stockfishproxy.controller;
 import com.pollock.stockfishproxy.dto.request.EngineAnalysisRequestDTO;
 import com.pollock.stockfishproxy.service.StockfishService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/pollock/engine/stockfish17.1")
 @RequiredArgsConstructor
@@ -20,9 +18,7 @@ public class StockfishController {
 
     @PostMapping
     public ResponseEntity<Void> publishEngineAnalysis(@RequestBody EngineAnalysisRequestDTO requestDTO) {
-        log.info("StockfishController.publishEngineAnalysis 요청 수신");
         stockfishService.publishEngineAnalysis(requestDTO);
-        log.info("StockfishController.publishEngineAnalysis 응답 완료");
         return ResponseEntity.accepted().build();
     }
 }
