f274b356b8f807f23fb4b59352c01e815ca15607
FickleBoBo <yukmw0704@gmail.com>
2025-05-21 11:36:50 +0900

    feat: 트래픽 정보 컴포넌트 임시 작성

---
+++ frontend/src/components/home/TrafficInfo.tsx
@@ -0,0 +1,16 @@
+const TrafficInfo = () => {
+  return (
+    <div className="flex flex-col gap-2 p-4">
+      <div className="flex justify-between">
+        <div>접속자 수</div>
+        <div>100,000</div>
+      </div>
+      <div className="flex justify-between">
+        <div>실시간 게임 수</div>
+        <div>30,000</div>
+      </div>
+    </div>
+  );
+};
+
+export default TrafficInfo;
