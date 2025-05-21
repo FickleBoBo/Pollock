6f7261a3bf371683f7e6fc64f083e1b5c919cdef
FickleBoBo <yukmw0704@gmail.com>
2025-05-21 11:41:22 +0900

    feat: 경기 기록 컴포넌트 임시 작성

---
+++ frontend/src/components/home/MatchHistory.tsx
@@ -0,0 +1,31 @@
+const MatchHistory = () => {
+  return (
+    <div className="bg-blue-600">
+      <div>경기 기록</div>
+      <div className="flex flex-col gap-2">
+        <div>
+          <div>11111</div>
+          <div>11111</div>
+        </div>
+        <div>
+          <div>22222</div>
+          <div>22222</div>
+        </div>
+        <div>
+          <div>33333</div>
+          <div>33333</div>
+        </div>
+        <div>
+          <div>44444</div>
+          <div>44444</div>
+        </div>
+        <div>
+          <div>55555</div>
+          <div>55555</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MatchHistory;
