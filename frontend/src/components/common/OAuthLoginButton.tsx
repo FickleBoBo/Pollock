46aeaab75cf26a35386199a85ab1a1f935e8bc8c
FickleBoBo <yukmw0704@gmail.com>
2025-05-21 11:40:29 +0900

    style: 버튼 너비 수정

--- frontend/src/components/common/OAuthLoginButton.tsx
+++ frontend/src/components/common/OAuthLoginButton.tsx
@@ -30,7 +30,7 @@ const OAuthLoginButton = ({ provider }: OAuthLoginButtonProps) => {
     <div>
       <a
         href={href}
-        className={`${className} inline-block w-[500px] text-center text-xl font-bold py-8 rounded`}
+        className={`${className} inline-block w-[400px] text-center text-xl font-bold py-8 rounded`}
       >
         {label}
       </a>
