10957e7c6823ae316fc67db8159575a77a084669
FickleBoBo <yukmw0704@gmail.com>
2025-05-21 11:49:47 +0900

    feat: 프로필 페이지 라우팅 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage";
+import ProfilePage from "./pages/profile/ProfilePage";
 import PuzzlePage from "./pages/puzzle/PuzzlePage";
 import OpeningPage from "./pages/learn/opening/OpeningPage";
 import AnalysisPage from "./assets/piece/temp/constant/AnalysisPage";
@@ -17,6 +18,8 @@ const App = () => {
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
+
+        <Route path="/profile" element={<ProfilePage />} />
 
         <Route path="/puzzle" element={<PuzzlePage />} />
         <Route path="/learn/opening" element={<OpeningPage />} />
