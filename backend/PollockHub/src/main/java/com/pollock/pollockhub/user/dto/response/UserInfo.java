a4a0c835d2d712223daedcd744e1bf97a9dc19cf
FickleBoBo <yukmw0704@gmail.com>
2025-05-21 11:34:37 +0900

    feat: elo 항목 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/dto/response/UserInfo.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/dto/response/UserInfo.java
@@ -11,14 +11,20 @@ public class UserInfo {
 
     private String nickname;
     private String profileImageUrl;
-    private Integer elo;
+    private Integer bulletElo;
+    private Integer blitzElo;
+    private Integer classicalElo;
+    private Integer puzzleElo;
     private Grade grade;
 
     public static UserInfo from(UserEntity userEntity) {
         return UserInfo.builder()
                 .nickname(userEntity.getNickname())
                 .profileImageUrl(userEntity.getProfileImageUrl())
-                .elo(userEntity.getElo())
+                .bulletElo(userEntity.getBulletElo())
+                .blitzElo(userEntity.getBlitzElo())
+                .classicalElo(userEntity.getClassicalElo())
+                .puzzleElo(userEntity.getPuzzleElo())
                 .grade(userEntity.getGrade())
                 .build();
     }
