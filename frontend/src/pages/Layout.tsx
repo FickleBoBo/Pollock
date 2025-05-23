f78d718b0800ff4dd26fce933736845223c4b12a
FickleBoBo <yukmw0704@gmail.com>
2025-05-23 20:23:06 +0900

    feat: 사이드바 레이아웃 페이지 작성

---
+++ frontend/src/pages/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+import SideMenu from "../components/common/SideMenu";
+
+const Layout = () => {
+  return (
+    <div>
+      <SideMenu />
+
+      <main className="ml-48">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
