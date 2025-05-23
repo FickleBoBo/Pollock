0869725aedae3010d18ba7437f598b966a3836a4
FickleBoBo <yukmw0704@gmail.com>
2025-05-23 20:25:50 +0900

    delete: 유저 타입 삭제

--- frontend/src/constant/User.ts
+++
@@ -1,9 +0,0 @@
-export interface UserInfo {
-  nickname: string;
-  profileImageUrl: string;
-  bulletElo: number;
-  blitzElo: number;
-  classicalElo: number;
-  puzzleElo: number;
-  grade: string;
-}
