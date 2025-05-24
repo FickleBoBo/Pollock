f08684e0c18c7bb1d4fa96e7a35b64524abc98d7
FickleBoBo <yukmw0704@gmail.com>
2025-05-24 09:55:21 +0900

    feat: 사이드 메뉴 넣은 레이아웃 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Layout from "./pages/Layout";
 import HomePage from "./pages/home/HomePage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import PuzzlePage from "./pages/puzzle/PuzzlePage";
 import OpeningPage from "./pages/learn/opening/OpeningPage";
-import AnalysisPage from "./assets/piece/temp/constant/AnalysisPage";
+import AnalysisPage from "./pages/learn/analysis/AnalysisPage";
 import NewsPage from "./pages/news/NewsPage";
 import FriendsPage from "./pages/social/friends/FriendsPage";
 import RankingPage from "./pages/ranking/RankingPage";
@@ -16,21 +17,24 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="home" element={<HomePage />} />
 
-        <Route path="/profile" element={<ProfilePage />} />
+          <Route path="profile" element={<ProfilePage />} />
 
-        <Route path="/puzzle" element={<PuzzlePage />} />
-        <Route path="/learn/opening" element={<OpeningPage />} />
-        <Route path="/learn/analysis" element={<AnalysisPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/social/friends" element={<FriendsPage />} />
-        <Route path="/ranking" element={<RankingPage />} />
-        <Route path="/store" element={<StorePage />} />
+          <Route path="puzzle" element={<PuzzlePage />} />
+          <Route path="learn/opening" element={<OpeningPage />} />
+          <Route path="learn/analysis" element={<AnalysisPage />} />
+          <Route path="learn/analysis/:gameId" element={<AnalysisPage />} />
+          <Route path="news" element={<NewsPage />} />
+          <Route path="social/friends" element={<FriendsPage />} />
+          <Route path="ranking" element={<RankingPage />} />
+          <Route path="store" element={<StorePage />} />
 
-        <Route path="/play" element={<PlayPage />} />
-        <Route path="/play/computer" element={<ComputerPage />} />
+          <Route path="play" element={<PlayPage />} />
+          <Route path="play/computer" element={<ComputerPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
