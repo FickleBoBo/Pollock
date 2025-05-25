3c14ae5803a577b264ac3a167525b7614d2ae68f
FickleBoBo <yukmw0704@gmail.com>
2025-05-25 17:04:55 +0900

    feat: oauthId, createAt 필드 추가 및 리팩토링

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/entity/UserEntity.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/entity/UserEntity.java
@@ -6,22 +6,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @Entity
 @Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity {
 
+    private static final int DEFAULT_ELO = 600;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
+    private String oauthId;
+
+    @Column
     private String email;
 
     @Column(nullable = false)
     private String nickname;
 
+    @Column(nullable = false)
     private String profileImageUrl;
 
     @Column(nullable = false)
@@ -46,15 +55,24 @@ public class UserEntity {
     @Column(nullable = false)
     private Grade grade;
 
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
+
     @Builder
-    public UserEntity(String email, String nickname, String profileImageUrl, Integer birthyear, Gender gender) {
+    public UserEntity(String oauthId, String email, String nickname, String profileImageUrl, Integer birthyear, Gender gender) {
+        this.oauthId = oauthId;
         this.email = email;
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
-        this.bulletElo = 600;
-        this.blitzElo = 600;
-        this.classicalElo = 600;
-        this.puzzleElo = 600;
+        this.bulletElo = DEFAULT_ELO;
+        this.blitzElo = DEFAULT_ELO;
+        this.classicalElo = DEFAULT_ELO;
+        this.puzzleElo = DEFAULT_ELO;
         this.birthyear = birthyear;
         this.gender = gender == null ? Gender.OTHER : gender;
         this.grade = Grade.BASIC;
