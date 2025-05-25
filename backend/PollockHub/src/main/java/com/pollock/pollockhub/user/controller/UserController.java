e1ce3f63cfc210070a1da69a5b1386557ea6924f
FickleBoBo <yukmw0704@gmail.com>
2025-05-26 04:48:38 +0900

    feat: 유저 정보 수정 엔드포인트 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.pollock.pollockhub.user.controller;
 
+import com.pollock.pollockhub.user.dto.request.UpdateUserInfoRequestDTO;
 import com.pollock.pollockhub.user.dto.response.UserInfoResponseDTO;
 import com.pollock.pollockhub.user.oauth2.annotation.Auth;
 import com.pollock.pollockhub.user.oauth2.dto.CustomOAuth2User;
 import com.pollock.pollockhub.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pollock/user")
@@ -20,5 +20,12 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<UserInfoResponseDTO> getUserInfo(@Auth CustomOAuth2User user) {
         return ResponseEntity.ok(userService.getUserInfo(user));
+    }
+
+    @PatchMapping("/me")
+    public ResponseEntity<UserInfoResponseDTO> updateUserInfo(@Auth CustomOAuth2User user,
+                                                              @RequestBody UpdateUserInfoRequestDTO requestDTO,
+                                                              HttpSession session) {
+        return ResponseEntity.ok(userService.updateUserInfo(user, requestDTO, session));
     }
 }
