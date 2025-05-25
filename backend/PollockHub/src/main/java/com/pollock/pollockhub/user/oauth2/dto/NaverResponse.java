82afaa89b1039a33ed8798dbe11c8f80f5062f2f
FickleBoBo <yukmw0704@gmail.com>
2025-05-25 17:06:16 +0900

    feat: 네이버 소셜 로그인 응답 클래스 NPE 처리 강화 및 oauthId 구현 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/dto/NaverResponse.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/dto/NaverResponse.java
@@ -3,8 +3,11 @@ package com.pollock.pollockhub.user.oauth2.dto;
 import com.pollock.pollockhub.user.entity.Gender;
 
 import java.util.Map;
+import java.util.UUID;
 
 public class NaverResponse implements OAuth2Response {
+
+    private static final String DEFAULT_PROFILE_IMAGE_URL = "https://avatars.githubusercontent.com/u/95597182?v=4";
 
     private final Map<String, Object> attributes;
 
@@ -14,35 +17,41 @@ public class NaverResponse implements OAuth2Response {
     }
 
     @Override
+    public String getOauthId() {
+        return "naver-" + attributes.get("id").toString();
+    }
+
+    @Override
     public String getEmail() {
-        return attributes.get("email").toString();
+        Object email = attributes.get("email");
+        return email != null ? email.toString() : null;
     }
 
     @Override
     public String getNickname() {
-        return attributes.get("nickname").toString();
+        Object nickname = attributes.get("nickname");
+        return nickname != null ? nickname.toString() : "guest-" + UUID.randomUUID();
     }
 
     @Override
     public String getProfileImageUrl() {
-        return attributes.get("profile_image").toString();
+        Object profileImageUrl = attributes.get("profile_image");
+        return profileImageUrl != null ? profileImageUrl.toString() : DEFAULT_PROFILE_IMAGE_URL;
     }
 
     @Override
     public Integer getBirthyear() {
-        return Integer.parseInt(attributes.get("birthyear").toString());
+        Object birthyear = attributes.get("birthyear");
+        return birthyear != null ? Integer.parseInt(birthyear.toString()) : null;
     }
 
     @Override
     public Gender getGender() {
-        String gender = attributes.get("gender").toString();
-
-        if (gender.equals("M")) {
-            return Gender.MALE;
-        } else if (gender.equals("F")) {
-            return Gender.FEMALE;
-        } else {
-            return Gender.OTHER;
+        Object gender = attributes.get("gender");
+        if (gender instanceof String) {
+            if (gender.toString().equals("M")) return Gender.MALE;
+            if (gender.toString().equals("F")) return Gender.FEMALE;
         }
+        return Gender.OTHER;
     }
 }
