cdcb0a26fd7de53566aa0a85661a39797753f93f
FickleBoBo <yukmw0704@gmail.com>
2025-05-26 05:30:45 +0900

    feat: 팩토리 타입 변경

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/config/RestTemplateConfig.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/config/RestTemplateConfig.java
@@ -2,7 +2,7 @@ package com.pollock.pollockhub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,7 +10,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(3000);
         factory.setReadTimeout(3000);
         return new RestTemplate(factory);
