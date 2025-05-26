67a00d24b643ee3f0a873a6a9418ed23511a1870
FickleBoBo <yukmw0704@gmail.com>
2025-05-27 07:47:26 +0900

    feat: 유저 검색 쿼리 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/repository/UserRepository.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.pollock.pollockhub.user.repository;
 import com.pollock.pollockhub.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
@@ -10,6 +11,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByOauthId(String oauthId);
 
     Optional<UserEntity> findByNickname(String nickname);
+
+    List<UserEntity> findByNicknameContainingOrderByNicknameAsc(String keyword);
 
     boolean existsByNickname(String nickname);
 }
