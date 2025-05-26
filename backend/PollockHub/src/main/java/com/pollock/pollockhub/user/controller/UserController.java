023974e3c86a0aec93450503f6a59785c72210f9
FickleBoBo <yukmw0704@gmail.com>
2025-05-27 04:47:28 +0900

    feat: 팔로우, 언팔로우, 팔로잉 조회, 팔로워 조회 API 엔드포인트 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.pollock.pollockhub.user.controller;
 
 import com.pollock.pollockhub.user.dto.request.UpdateUserProfileRequestDTO;
 import com.pollock.pollockhub.user.dto.response.UserInfoResponseDTO;
+import com.pollock.pollockhub.user.dto.response.UserSimpleInfoResponseDTO;
 import com.pollock.pollockhub.user.oauth2.annotation.Auth;
 import com.pollock.pollockhub.user.oauth2.dto.CustomOAuth2User;
 import com.pollock.pollockhub.user.service.UserService;
@@ -9,6 +10,8 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pollock/user")
@@ -32,5 +35,29 @@ public class UserController {
     @GetMapping("/nickname/check")
     public ResponseEntity<Boolean> checkNicknameDuplicate(@RequestParam String nickname) {
         return ResponseEntity.ok(userService.checkNicknameDuplicate(nickname));
+    }
+
+    @PostMapping("/follow/{followeeNickname}")
+    public ResponseEntity<Void> follow(@Auth CustomOAuth2User user,
+                                       @PathVariable String followeeNickname) {
+        userService.follow(user, followeeNickname);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/follow/{followeeNickname}")
+    public ResponseEntity<Void> unfollow(@Auth CustomOAuth2User user,
+                                         @PathVariable String followeeNickname) {
+        userService.unfollow(user, followeeNickname);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/following")
+    public ResponseEntity<List<UserSimpleInfoResponseDTO>> getFollowing(@Auth CustomOAuth2User user) {
+        return ResponseEntity.ok(userService.getFollowing(user));
+    }
+
+    @GetMapping("/followers")
+    public ResponseEntity<List<UserSimpleInfoResponseDTO>> getFollowers(@Auth CustomOAuth2User user) {
+        return ResponseEntity.ok(userService.getFollowers(user));
     }
 }
