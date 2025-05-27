bea114ffc02ba1565624c4eb4adcacfc1c0e4932
FickleBoBo <yukmw0704@gmail.com>
2025-05-27 14:59:28 +0900

    feat: 정적 팩터리 메서드 작성 및 지연 초기화 예외 문제 처리

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/dto/response/UserInfoResponseDTO.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/dto/response/UserInfoResponseDTO.java
@@ -3,6 +3,7 @@ package com.pollock.pollockhub.user.dto.response;
 import com.pollock.pollockhub.user.entity.Gender;
 import com.pollock.pollockhub.user.entity.Role;
 import com.pollock.pollockhub.user.entity.Title;
+import com.pollock.pollockhub.user.entity.UserEntity;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -27,6 +28,25 @@ public class UserInfoResponseDTO {
     private Role role;
     private Title title;
     private LocalDateTime createdAt;
-    private Integer followingCount;
-    private Integer followersCount;
+    private Long followingCount;
+    private Long followersCount;
+
+    public static UserInfoResponseDTO from(UserEntity userEntity, long followingCount, long followersCount) {
+        return UserInfoResponseDTO.builder()
+                .email(userEntity.getEmail())
+                .nickname(userEntity.getNickname())
+                .profileImageUrl(userEntity.getProfileImageUrl())
+                .bulletElo(userEntity.getBulletElo())
+                .blitzElo(userEntity.getBlitzElo())
+                .classicalElo(userEntity.getClassicalElo())
+                .puzzleElo(userEntity.getPuzzleElo())
+                .birthyear(userEntity.getBirthyear())
+                .gender(userEntity.getGender())
+                .role(userEntity.getRole())
+                .title(userEntity.getTitle())
+                .createdAt(userEntity.getCreatedAt())
+                .followingCount(followingCount)
+                .followersCount(followersCount)
+                .build();
+    }
 }
