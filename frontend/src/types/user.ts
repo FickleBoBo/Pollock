4fd22684c165f0190c329cc6d529e59781317781
FickleBoBo <yukmw0704@gmail.com>
2025-05-27 09:43:21 +0900

    feat: 유저 관련 타입, 인터페이스 작성

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,18 @@
+export type Gender = "MALE" | "FEMALE" | "OTHER";
+
+export type Role = "BASIC" | "PREMIUM" | "ADMIN";
+
+export type Title = "GM" | "IM" | "FM" | "CM" | "NONE";
+
+export interface UserSimpleInfo {
+  nickname: string;
+  profileImageUrl: string;
+  bulletElo: number;
+  blitzElo: number;
+  classicalElo: number;
+  puzzleElo: number;
+  role: Role;
+  title: Title;
+  followingCount: number;
+  followersCount: number;
+}
