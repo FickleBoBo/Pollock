40cd548b0781d622c1c09ef0bba4d74f50167aa6
FickleBoBo <yukmw0704@gmail.com>
2025-05-28 21:45:01 +0900

    style: 개행 제거

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
@@ -23,8 +23,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@RequestBody UserSignupRequestDTO requestDTO,
-                                       HttpSession session) {
+    public ResponseEntity<Void> signup(@RequestBody UserSignupRequestDTO requestDTO, HttpSession session) {
         userService.signup(requestDTO, session);
         return ResponseEntity.ok().build();
     }
@@ -35,8 +34,7 @@ public class UserController {
     }
 
     @PutMapping("/me")
-    public ResponseEntity<UserInfoResponseDTO> updateUserProfile(@Auth CustomOAuth2User user,
-                                                                 @RequestBody UpdateUserProfileRequestDTO requestDTO) {
+    public ResponseEntity<UserInfoResponseDTO> updateUserProfile(@Auth CustomOAuth2User user, @RequestBody UpdateUserProfileRequestDTO requestDTO) {
         return ResponseEntity.ok(userService.updateUserProfile(user, requestDTO));
     }
 
@@ -46,34 +44,29 @@ public class UserController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> searchUsers(@RequestParam String keyword,
-                                                                       @PageableDefault(sort = "nickname") Pageable pageable) {
+    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> searchUsers(@RequestParam String keyword, @PageableDefault(sort = "nickname") Pageable pageable) {
         return ResponseEntity.ok(userService.searchUsers(keyword, pageable));
     }
 
     @PostMapping("/follow/{followeeNickname}")
-    public ResponseEntity<Void> follow(@Auth CustomOAuth2User user,
-                                       @PathVariable String followeeNickname) {
+    public ResponseEntity<Void> follow(@Auth CustomOAuth2User user, @PathVariable String followeeNickname) {
         userService.follow(user, followeeNickname);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/follow/{followeeNickname}")
-    public ResponseEntity<Void> unfollow(@Auth CustomOAuth2User user,
-                                         @PathVariable String followeeNickname) {
+    public ResponseEntity<Void> unfollow(@Auth CustomOAuth2User user, @PathVariable String followeeNickname) {
         userService.unfollow(user, followeeNickname);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/following")
-    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> getFollowing(@Auth CustomOAuth2User user,
-                                                                        @PageableDefault(sort = "nickname") Pageable pageable) {
+    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> getFollowing(@Auth CustomOAuth2User user, @PageableDefault(sort = "nickname") Pageable pageable) {
         return ResponseEntity.ok(userService.getFollowing(user, pageable));
     }
 
     @GetMapping("/followers")
-    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> getFollowers(@Auth CustomOAuth2User user,
-                                                                        @PageableDefault(sort = "nickname") Pageable pageable) {
+    public ResponseEntity<Page<UserSimpleInfoResponseDTO>> getFollowers(@Auth CustomOAuth2User user, @PageableDefault(sort = "nickname") Pageable pageable) {
         return ResponseEntity.ok(userService.getFollowers(user, pageable));
     }
 }
