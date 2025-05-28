c2dd863bb9cc30c35a2110924b110186cb951ff1
FickleBoBo <yukmw0704@gmail.com>
2025-05-28 21:36:45 +0900

    feat: 전역 상수 클래스 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.pollock.pollockhub.constant;
+
+public class Constant {
+
+    // User Domain
+    public static final String DEFAULT_PROFILE_IMAGE_URL = "https://avatars.githubusercontent.com/u/95597182?v=4";
+    public static final int DEFAULT_ELO = 600;
+
+    public static final int MIN_NICKNAME_LENGTH = 4;
+    public static final int MAX_NICKNAME_LENGTH = 16;
+}
