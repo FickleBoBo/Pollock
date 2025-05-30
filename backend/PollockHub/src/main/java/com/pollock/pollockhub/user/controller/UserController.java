8864bc7442a47746b6218d8c08ff24576b39a2a6
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 21:41:55 +0900

    feat: public 컨트롤러 분리

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.pollock.pollockhub.user.controller;
 
 import com.pollock.pollockhub.user.dto.request.UpdateUserProfileRequestDTO;
-import com.pollock.pollockhub.user.dto.request.UserSignupRequestDTO;
 import com.pollock.pollockhub.user.dto.response.UserPrivateInfoResponseDTO;
 import com.pollock.pollockhub.user.dto.response.UserPublicInfoResponseDTO;
 import com.pollock.pollockhub.user.oauth2.annotation.Auth;
 import com.pollock.pollockhub.user.oauth2.dto.CustomOAuth2User;
 import com.pollock.pollockhub.user.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,39 +13,12 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/api/pollock/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping
-    public ResponseEntity<Void> signup(@RequestBody UserSignupRequestDTO requestDTO,
-                                       HttpSession session) {
-        userService.signup(requestDTO, session);
-        return ResponseEntity.created(URI.create("/api/pollock/users/me")).build();
-    }
-
-    @GetMapping
-    public ResponseEntity<Page<UserPublicInfoResponseDTO>> getUsers(@RequestParam(defaultValue = "") String keyword,
-                                                                    @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(userService.getUsers(keyword, pageable));
-    }
-
-    @GetMapping("/{nickname}")
-    public ResponseEntity<UserPublicInfoResponseDTO> getUser(@PathVariable String nickname) {
-        return ResponseEntity.ok(userService.getUser(nickname));
-    }
-
-    @RequestMapping(value = "/{nickname}", method = RequestMethod.HEAD)
-    public ResponseEntity<Void> checkNicknameExists(@PathVariable String nickname) {
-        return userService.checkNicknameExists(nickname)
-                ? ResponseEntity.ok().build()
-                : ResponseEntity.notFound().build();
-    }
 
     @GetMapping("/me")
     public ResponseEntity<UserPrivateInfoResponseDTO> getMyInfo(@Auth CustomOAuth2User user) {
@@ -84,17 +55,5 @@ public class UserController {
     public ResponseEntity<Page<UserPublicInfoResponseDTO>> getMyFollowers(@Auth CustomOAuth2User user,
                                                                           @PageableDefault Pageable pageable) {
         return ResponseEntity.ok(userService.getMyFollowers(user, pageable));
-    }
-
-    @GetMapping("/{nickname}/following")
-    public ResponseEntity<Page<UserPublicInfoResponseDTO>> getUserFollowing(@PathVariable String nickname,
-                                                                            @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(userService.getUserFollowing(nickname, pageable));
-    }
-
-    @GetMapping("/{nickname}/followers")
-    public ResponseEntity<Page<UserPublicInfoResponseDTO>> getUserFollowers(@PathVariable String nickname,
-                                                                            @PageableDefault Pageable pageable) {
-        return ResponseEntity.ok(userService.getUserFollowers(nickname, pageable));
     }
 }
