3dab51cd078c2df684ae3da4cd351cd03cf47f2c
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 21:05:45 +0900

    feat: ToString 어노테이션 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/websocket/subscriber/engine_analysis/dto/response/EngineAnalysisResponseDTO.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/websocket/subscriber/engine_analysis/dto/response/EngineAnalysisResponseDTO.java
@@ -1,10 +1,12 @@
 package com.pollock.pollockhub.websocket.subscriber.engine_analysis.dto.response;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@ToString
 public class EngineAnalysisResponseDTO {
 
     private Integer score;
