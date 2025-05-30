708555f8a5bad53ce40d4dd8d21c26a189cacdf9
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 21:43:33 +0900

    feat: 커스텀 로그아웃 핸들러 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/handler/CustomLogoutSuccessHandler.java
@@ -0,0 +1,19 @@
+package com.pollock.pollockhub.user.oauth2.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Authentication authentication) {
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
