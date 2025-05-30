426ac182df6722643a5923ef201eae6930604e69
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 17:04:15 +0900

    style: 하이퍼링크 경로 로컬로 수정

--- frontend/src/components/common/OAuthLoginButton.tsx
+++ frontend/src/components/common/OAuthLoginButton.tsx
@@ -5,20 +5,20 @@ interface OAuthLoginButtonProps {
 const OAuthLoginButton = ({ provider }: OAuthLoginButtonProps) => {
   const config = {
     naver: {
-      // href="http://www.pollock.kr/api/pollock/user/oauth2/authorization/naver"
-      href: "http://localhost:8080/api/pollock/user/oauth2/authorization/naver",
+      // href="http://www.pollock.kr/api/pollock/users/oauth2/authorization/naver"
+      href: "http://localhost:8080/api/pollock/users/oauth2/authorization/naver",
       className: "bg-[#03C75A] hover:bg-[#029E4F] text-white",
       label: "Naver 로그인",
     },
     kakao: {
-      // href="http://www.pollock.kr/api/pollock/user/oauth2/authorization/kakao"
-      href: "http://localhost:8080/api/pollock/user/oauth2/authorization/kakao",
+      // href="http://www.pollock.kr/api/pollock/users/oauth2/authorization/kakao"
+      href: "http://localhost:8080/api/pollock/users/oauth2/authorization/kakao",
       className: "bg-[#FEE500] hover:bg-[#ECD900] text-black",
       label: "Kakao 로그인",
     },
     google: {
-      // href="http://www.pollock.kr/api/pollock/user/oauth2/authorization/google"
-      href: "http://localhost:8080/api/pollock/user/oauth2/authorization/google",
+      // href="http://www.pollock.kr/api/pollock/users/oauth2/authorization/google"
+      href: "http://localhost:8080/api/pollock/users/oauth2/authorization/google",
       className: "bg-white hover:bg-[#E2E2E2] text-black",
       label: "Google 로그인",
     },
