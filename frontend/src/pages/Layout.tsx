4a53cdd32bc02f7f12c6ad425a1b107faf84c365
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 15:13:34 +0900

    style: import 절대 경로로 수정

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import SideMenu from "../components/common/SideMenu";
+import SideMenu from "@/components/common/desktop/SideMenu";
 
 const Layout = () => {
   return (
