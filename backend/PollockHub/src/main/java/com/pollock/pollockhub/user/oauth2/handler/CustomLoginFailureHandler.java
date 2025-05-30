c2c7d0756a564992c1c81d35bd497d6a3e3aae11
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 21:43:41 +0900

    feat: 커스텀 로그인 핸들러 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/handler/CustomLoginFailureHandler.java
@@ -0,0 +1,19 @@
+package com.pollock.pollockhub.user.oauth2.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomLoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
