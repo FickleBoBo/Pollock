51be700f4d93e979707029908973cddadc3e797c
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 20:46:13 +0900

    feat: ToString 어노테이션 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/dto/request/EngineAnalysisRequestDTO.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/engine/dto/request/EngineAnalysisRequestDTO.java
@@ -2,8 +2,10 @@ package com.pollock.pollockhub.engine.dto.request;
 
 import com.pollock.pollockhub.engine.router.EngineType;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class EngineAnalysisRequestDTO {
 
     private String channelKey;
