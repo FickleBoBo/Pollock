7d63d30d033dc86c599e407ce97269433fa5bfbc
FickleBoBo <yukmw0704@gmail.com>
2025-05-30 20:56:40 +0900

    feat: 기본 생성자 차단 및 경로 수정

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/constant/ChannelConstant.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/constant/ChannelConstant.java
@@ -1,11 +1,18 @@
 package com.pollock.pollockhub.constant;
 
 public class ChannelConstant {
-    public static final String TOPIC_SESSION_COUNT = "/topic/sessions";
-    public static final String TOPIC_GAME_COUNT = "/topic/games";
 
+    private ChannelConstant() {
+    }
+
+    // WebSocket Topics
     public static final String TOPIC_PING = "/topic/ping/%s";
+
+    public static final String TOPIC_SESSION_COUNT = "/topic/session-count";
+    public static final String TOPIC_GAME_COUNT = "/topic/game-count";
+
     public static final String TOPIC_ENGINE_ANALYSIS = "/topic/analysis/%s";
 
+    // Redis Pub/Sub Channels
     public static final String SUBSCRIBE_ENGINE_ANALYSIS_PATTERN = "engine:*";
 }
