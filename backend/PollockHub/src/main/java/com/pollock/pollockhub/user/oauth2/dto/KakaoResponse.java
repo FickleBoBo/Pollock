8661f6db1ac6b548e5c9c6569cc1b90e398bbd19
FickleBoBo <yukmw0704@gmail.com>
2025-06-02 19:13:29 +0900

    feat: KAKAO DTO 변경

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/dto/KakaoResponse.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/oauth2/dto/KakaoResponse.java
@@ -1,8 +1,12 @@
 package com.pollock.pollockhub.user.oauth2.dto;
 
 import com.pollock.pollockhub.user.entity.Gender;
+import com.pollock.pollockhub.user.oauth2.enums.OAuth2Provider;
 
 import java.util.Map;
+
+import static com.pollock.pollockhub.user.entity.Gender.OTHER;
+import static com.pollock.pollockhub.user.oauth2.enums.OAuth2Provider.KAKAO;
 
 public class KakaoResponse implements OAuth2Response {
 
@@ -12,9 +16,13 @@ public class KakaoResponse implements OAuth2Response {
         this.attributes = attributes;
     }
 
+    public OAuth2Provider getOAuth2Provider() {
+        return KAKAO;
+    }
+
     @Override
-    public String getOauthId() {
-        return "kakao-" + attributes.get("id").toString();
+    public String getOAuth2ProviderId() {
+        return attributes.get("id").toString();
     }
 
     @Override
@@ -28,7 +36,17 @@ public class KakaoResponse implements OAuth2Response {
     }
 
     @Override
+    public Integer getBirthmonth() {
+        return null;
+    }
+
+    @Override
+    public Integer getBirthday() {
+        return null;
+    }
+
+    @Override
     public Gender getGender() {
-        return Gender.OTHER;
+        return OTHER;
     }
 }
