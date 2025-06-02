7b96cae9a44e45999a340e958bd10dffa6a9a520
FickleBoBo <yukmw0704@gmail.com>
2025-06-02 19:54:05 +0900

    feat: 게임 타입 enum 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/game/enums/GameType.java
@@ -0,0 +1,8 @@
+package com.pollock.pollockhub.game.enums;
+
+public enum GameType {
+    BULLET_1_0, BULLET_1_1, BULLET_2_1,
+    BLITZ_3_0, BLITZ_3_2, BLITZ_5_0,
+    RAPID_10_0, RAPID_10_5, RAPID_15_10,
+    CLASSICAL_30_0, CLASSICAL_30_20, CLASSICAL_90_30
+}
