866379b174dc5001e48e004d0f3cf4d80324b358
FickleBoBo <yukmw0704@gmail.com>
2025-06-02 19:14:23 +0900

    feat: 소셜키 조회 쿼리 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/user/repository/UserRepository.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/user/repository/UserRepository.java
@@ -1,15 +1,19 @@
 package com.pollock.pollockhub.user.repository;
 
 import com.pollock.pollockhub.user.entity.UserEntity;
+import com.pollock.pollockhub.user.oauth2.enums.OAuth2Provider;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByOauthId(String oauthId);
+    @Query("SELECT u FROM UserEntity u WHERE u.oAuth2Provider = :oAuth2Provider AND u.oAuth2ProviderId = :oAuth2ProviderId")
+    Optional<UserEntity> findByOAuth2ProviderAndOAuth2ProviderId(@Param("oAuth2Provider") OAuth2Provider oAuth2Provider, @Param("oAuth2ProviderId") String oAuth2ProviderId);
 
     Optional<UserEntity> findByNickname(String nickname);
 
