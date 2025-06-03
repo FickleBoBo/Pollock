6598c35a8985928f9b35ba4ef986c7a12f005a0d
FickleBoBo <yukmw0704@gmail.com>
2025-06-03 23:38:24 +0900

    feat: 레디스 매칭큐 삽입 메서드 작성

---
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/game/service/GameService.java
@@ -0,0 +1,30 @@
+package com.pollock.pollockhub.game.service;
+
+import com.pollock.pollockhub.game.enums.GameType;
+import com.pollock.pollockhub.user.oauth2.dto.CustomOAuth2User;
+import com.pollock.pollockhub.user.service.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import static com.pollock.pollockhub.constant.ChannelConstant.MATCH_KEY;
+
+@Service
+public class GameService {
+
+    private final UserService userService;
+    private final StringRedisTemplate matchEventRedisTemplate;
+
+    public GameService(UserService userService,
+                       @Qualifier("matchEventRedisTemplate") StringRedisTemplate matchEventRedisTemplate) {
+        this.userService = userService;
+        this.matchEventRedisTemplate = matchEventRedisTemplate;
+    }
+
+    public void enqueueUser(CustomOAuth2User user, GameType gameType) {
+        int eloBucket = userService.getEloByGameType(user, gameType) / 10 * 10;
+
+        String queueKey = String.format(MATCH_KEY, gameType, eloBucket);
+        matchEventRedisTemplate.opsForList().rightPush(queueKey, user.getNickname());
+    }
+}
