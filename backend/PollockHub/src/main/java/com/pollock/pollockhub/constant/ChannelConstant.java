e09ca05de417ed8956e0f01c4e6055c6ceec474a
FickleBoBo <yukmw0704@gmail.com>
2025-06-03 23:23:52 +0900

    feat: 게임 토픽 추가

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/constant/ChannelConstant.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/constant/ChannelConstant.java
@@ -11,12 +11,14 @@ public class ChannelConstant {
     public static final String TOPIC_SESSION_COUNT = "/topic/session/count";
     public static final String TOPIC_GAME_COUNT = "/topic/game/count";
 
+    public static final String TOPIC_GAME = "/topic/game/%s";
+
     public static final String TOPIC_MATCH = "/topic/match/%s";
 
     public static final String TOPIC_ENGINE_ANALYSIS = "/topic/engine/analysis/%s";
 
     // Redis Key
-    public static final String MATCH_QUEUE_KEY = "match_queue:%s:%d";
+    public static final String MATCH_KEY = "match:%s:%d";
 
     // Redis Pub/Sub Pattern
     public static final String SUBSCRIBE_ENGINE_ANALYSIS_PATTERN = "engine:*";
