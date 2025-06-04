a0aae879d0e5d5d0354f5e514401cea9d455fe0b
FickleBoBo <yukmw0704@gmail.com>
2025-06-05 00:13:29 +0900

    init: 프로젝트 생성

---
+++ lichess/LichessAnalytics/src/main/java/com/pollock/lichessanalytics/LichessAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.pollock.lichessanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LichessAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LichessAnalyticsApplication.class, args);
+    }
+
+}
---
+++ lichess/LichessAnalytics/src/test/java/com/pollock/lichessanalytics/LichessAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollock.lichessanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LichessAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
