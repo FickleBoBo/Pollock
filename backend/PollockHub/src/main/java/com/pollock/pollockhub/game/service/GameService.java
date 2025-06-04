5c1b3d47ba05d84707b7f46902c5c8eadd0297bf
FickleBoBo <yukmw0704@gmail.com>
2025-06-04 22:55:40 +0900

    feat: 게임 조회 서비스 메서드 작성

--- backend/PollockHub/src/main/java/com/pollock/pollockhub/game/service/GameService.java
+++ backend/PollockHub/src/main/java/com/pollock/pollockhub/game/service/GameService.java
@@ -1,9 +1,15 @@
 package com.pollock.pollockhub.game.service;
 
+import com.pollock.pollockhub.game.dto.response.GameInfoResponseDTO;
+import com.pollock.pollockhub.game.entity.GameEntity;
 import com.pollock.pollockhub.game.entity.GameType;
+import com.pollock.pollockhub.game.exception.GameNotFoundException;
+import com.pollock.pollockhub.game.repository.GameRepository;
 import com.pollock.pollockhub.user.oauth2.dto.CustomOAuth2User;
 import com.pollock.pollockhub.user.service.UserService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +20,14 @@ import static com.pollock.pollockhub.constant.Constant.ELO_BUCKET_SIZE;
 public class GameService {
 
     private final UserService userService;
+    private final GameRepository gameRepository;
     private final StringRedisTemplate matchEventRedisTemplate;
 
     public GameService(UserService userService,
+                       GameRepository gameRepository,
                        @Qualifier("matchEventRedisTemplate") StringRedisTemplate matchEventRedisTemplate) {
         this.userService = userService;
+        this.gameRepository = gameRepository;
         this.matchEventRedisTemplate = matchEventRedisTemplate;
     }
 
@@ -27,5 +36,18 @@ public class GameService {
 
         String queueKey = String.format(MATCH_KEY, gameType, eloBucket);
         matchEventRedisTemplate.opsForList().rightPush(queueKey, user.getNickname());
+    }
+
+    public Page<GameInfoResponseDTO> getGames(Pageable pageable) {
+        return gameRepository.findAll(pageable)
+                .map(GameInfoResponseDTO::from);
+    }
+
+    public GameInfoResponseDTO getGame(Long gameId) {
+        return GameInfoResponseDTO.from(getGameEntity(gameId));
+    }
+
+    private GameEntity getGameEntity(Long id) {
+        return gameRepository.findById(id).orElseThrow(GameNotFoundException::getInstance);
     }
 }
