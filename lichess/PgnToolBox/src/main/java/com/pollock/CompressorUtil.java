a3fedf5805d8663e974c4287c183e48a6e3ad923
FickleBoBo <yukmw0704@gmail.com>
2025-06-06 20:23:38 +0900

    feat: eval 태그만 포함하는 스트리밍 메서드 작성

--- lichess/PgnToolBox/src/main/java/com/pollock/CompressorUtil.java
+++ lichess/PgnToolBox/src/main/java/com/pollock/CompressorUtil.java
@@ -9,8 +9,8 @@ import java.nio.file.Path;
 
 public class CompressorUtil {
 
-    private static final String SITE_PREFIX = "[Site \"https://lichess.org/";
     private static final String PGN_PREFIX = "1.";
+    private static final String EVAL_TAG = "eval";
 
     private CompressorUtil() {
     }
@@ -64,68 +64,52 @@ public class CompressorUtil {
         }
     }
 
-    public static void extract(Path input, Path outputDir) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new ZstdInputStream(new FileInputStream(input.toFile()))));
+    public static void filteringEvalGame(Path input, Path output) throws IOException {
+        try (var br = new BufferedReader(new InputStreamReader(new ZstdInputStream(Files.newInputStream(input))));
+             var bw = new BufferedWriter(new OutputStreamWriter(new ZstdOutputStream(Files.newOutputStream(output))))) {
 
-        String pgnName = null;
-        StringBuilder pgnDataBuilder = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
-        boolean engineEvalFlag = false;
+            String line = null;
+            int emptyLineCnt = 0;
 
-        String line = null;
-        int emptyLineCnt = 0;
+            boolean isEmptyGame = true;
 
-        int splitGameCnt = 0;
-        int totalGameCnt = 0;
+            int streamedGameCnt = 0;
+            int totalGameCnt = 0;
 
-        while ((line = br.readLine()) != null) {
-            pgnDataBuilder.append(line).append("\n");
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
 
-            if (line.startsWith(SITE_PREFIX)) {
-                int startIdx = SITE_PREFIX.length();
-                int endIdx = line.indexOf('"', startIdx);
-                pgnName = line.substring(startIdx, endIdx);
-            }
+                if (line.startsWith(PGN_PREFIX) && line.contains(EVAL_TAG)) {
+                    isEmptyGame = false;
+                }
 
-            if (line.startsWith(PGN_PREFIX)) {
-                if (line.contains("eval")) {
-                    engineEvalFlag = true;
+                if (line.isEmpty()) emptyLineCnt++;
+
+                if (emptyLineCnt > 1) {
+                    if (isEmptyGame) {
+                    } else {
+                        bw.write(sb.toString());
+                        streamedGameCnt++;
+                    }
+
+                    emptyLineCnt = 0;
+                    isEmptyGame = true;
+                    totalGameCnt++;
+                    sb.setLength(0);
+
+                    if (totalGameCnt % 1_000_000 == 0) {
+                        System.out.printf("Processing StreamedGameCnt = %,d\n", streamedGameCnt);
+                        System.out.printf("Processing TotalGameCnt = %,d\n", totalGameCnt);
+                        System.out.printf("Processing StreamedGame Ratio = %.4f\n\n", (streamedGameCnt * 100.0) / totalGameCnt);
+                    }
                 }
             }
 
-            if (line.isEmpty()) {
-                emptyLineCnt++;
-            }
-
-            if (emptyLineCnt > 1) {
-                totalGameCnt++;
-                emptyLineCnt = 0;
-
-                if (!engineEvalFlag) {
-                    pgnName = null;
-                    pgnDataBuilder.setLength(0);
-                    continue;
-                }
-
-                File dir = new File(String.valueOf(outputDir));
-                if (!dir.exists()) dir.mkdirs();
-
-                BufferedWriter bw = new BufferedWriter(new FileWriter(new File(dir, pgnName + ".pgn")));
-                bw.write(pgnDataBuilder.toString());
-                bw.flush();
-
-                splitGameCnt++;
-                if (splitGameCnt % 10_000 == 0) {
-                    System.out.printf("Processing %,d entries...\n", splitGameCnt);
-                }
-
-                pgnName = null;
-                pgnDataBuilder.setLength(0);
-                engineEvalFlag = false;
-            }
+            System.out.printf("StreamedGameCnt = %,d\n", streamedGameCnt);
+            System.out.printf("TotalGameCnt = %,d\n", totalGameCnt);
+            System.out.printf("StreamedGame Ratio = %.4f\n\n", (streamedGameCnt * 100.0) / totalGameCnt);
         }
-
-        System.out.printf("splitGameCnt = %,d\n", splitGameCnt);
-        System.out.printf("totalGameCnt = %,d\n", totalGameCnt);
     }
 }
