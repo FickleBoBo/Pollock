31109efda62a74279ea7fce5b95ec20f1a5bb1d3
FickleBoBo <yukmw0704@gmail.com>
2025-06-06 20:11:58 +0900

    feat: 더미 게임 명령어 케이스 작성

--- lichess/PgnToolBox/src/main/java/com/pollock/Main.java
+++ lichess/PgnToolBox/src/main/java/com/pollock/Main.java
@@ -6,6 +6,17 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws IOException {
         switch (args[0]) {
+            case "filteringEmptyGame" -> {
+                if (args.length != 3) {
+                    System.out.println("Usage: filteringEmptyGame <input.zst> <output.zst>");
+                    return;
+                }
+
+                Path input = Path.of(args[1]);
+                Path output = Path.of(args[2]);
+
+                CompressorUtil.filteringEmptyGame(input, output);
+            }
             case "extract" -> {
                 if (args.length != 3) {
                     System.out.println("Usage: extract <input.zst> <outputDir>");
