f276b7b6c183402f5240853b1cbf3a48d8014ea5
FickleBoBo <yukmw0704@gmail.com>
2025-06-06 20:12:13 +0900

    feat: 더미 게임 제거 스트리밍 메서드 작성

--- lichess/PgnToolBox/src/main/java/com/pollock/CompressorUtil.java
+++ lichess/PgnToolBox/src/main/java/com/pollock/CompressorUtil.java
@@ -1,8 +1,10 @@
 package com.pollock;
 
 import com.github.luben.zstd.ZstdInputStream;
+import com.github.luben.zstd.ZstdOutputStream;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class CompressorUtil {
@@ -11,6 +13,55 @@ public class CompressorUtil {
     private static final String PGN_PREFIX = "1.";
 
     private CompressorUtil() {
+    }
+
+    public static void filteringEmptyGame(Path input, Path output) throws IOException {
+        try (var br = new BufferedReader(new InputStreamReader(new ZstdInputStream(Files.newInputStream(input))));
+             var bw = new BufferedWriter(new OutputStreamWriter(new ZstdOutputStream(Files.newOutputStream(output))))) {
+
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            int emptyLineCnt = 0;
+
+            boolean isEmptyGame = true;
+
+            int streamedGameCnt = 0;
+            int totalGameCnt = 0;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+
+                if (line.startsWith(PGN_PREFIX)) {
+                    isEmptyGame = false;
+                }
+
+                if (line.isEmpty()) emptyLineCnt++;
+
+                if (emptyLineCnt > 1) {
+                    if (isEmptyGame) {
+                    } else {
+                        bw.write(sb.toString());
+                        streamedGameCnt++;
+                    }
+
+                    emptyLineCnt = 0;
+                    isEmptyGame = true;
+                    totalGameCnt++;
+                    sb.setLength(0);
+
+                    if (totalGameCnt % 1_000_000 == 0) {
+                        System.out.printf("Processing StreamedGameCnt = %,d\n", streamedGameCnt);
+                        System.out.printf("Processing TotalGameCnt = %,d\n", totalGameCnt);
+                        System.out.printf("Processing StreamedGame Ratio = %.4f\n\n", (streamedGameCnt * 100.0) / totalGameCnt);
+                    }
+                }
+            }
+
+            System.out.printf("StreamedGameCnt = %,d\n", streamedGameCnt);
+            System.out.printf("TotalGameCnt = %,d\n", totalGameCnt);
+            System.out.printf("StreamedGame Ratio = %.4f\n\n", (streamedGameCnt * 100.0) / totalGameCnt);
+        }
     }
 
     public static void extract(Path input, Path outputDir) throws IOException {
