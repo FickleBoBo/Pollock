fb4e63d44cffde308a438d25444084d61468952b
FickleBoBo <yukmw0704@gmail.com>
2025-06-09 01:06:25 +0900

    feat: 압축률 변경 커멘트 작성

--- lichess/PgnToolBox/src/main/java/com/pollock/Main.java
+++ lichess/PgnToolBox/src/main/java/com/pollock/Main.java
@@ -31,6 +31,31 @@ public class Main {
 
                 CompressorUtil.filter(input, output, level);
             }
+            case "recompress" -> {
+                if (args.length < 2 || args.length > 3) {
+                    System.out.println("Usage: recompress <input.zst> [-level(1-22)]");
+                    return;
+                }
+
+                int level = 3;
+                if (args.length == 3) {
+                    try {
+                        level = Integer.parseInt(args[2].substring(1));
+
+                        if (level < 1 || level > 22) {
+                            throw new NumberFormatException();
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid compression level(1-22): " + args[2]);
+                        return;
+                    }
+                }
+
+                Path input = Path.of(args[1]);
+                Path output = input.getParent().resolve(input.getFileName().toString().replaceFirst("\\.pgn\\.zst$", "_filter_" + level + ".pgn.zst"));
+
+                CompressorUtil.recompress(input, output, level);
+            }
             default -> System.out.println("Unknown command: " + args[0]);
         }
     }
