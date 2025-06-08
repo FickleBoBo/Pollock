56f9a34918bf15ee31de77ae9e23d681212eae11
FickleBoBo <yukmw0704@gmail.com>
2025-06-08 22:45:38 +0900

    feat: 빈게임 필터링 커멘드, eval 게임 필터링 커멘드 제거 및 커멘드 이름 변경

--- lichess/PgnToolBox/src/main/java/com/pollock/Main.java
+++ lichess/PgnToolBox/src/main/java/com/pollock/Main.java
@@ -6,38 +6,16 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws IOException {
         switch (args[0]) {
-            case "filteringEmptyGame" -> {
+            case "filter" -> {
                 if (args.length != 3) {
-                    System.out.println("Usage: filteringEmptyGame <input.zst> <output.zst>");
+                    System.out.println("Usage: filter <input.zst> <output.zst>");
                     return;
                 }
 
                 Path input = Path.of(args[1]);
                 Path output = Path.of(args[2]);
 
-                CompressorUtil.filteringEmptyGame(input, output);
-            }
-            case "filteringEvalGame" -> {
-                if (args.length != 3) {
-                    System.out.println("Usage: filteringEvalGame <input.zst> <output.zst>");
-                    return;
-                }
-
-                Path input = Path.of(args[1]);
-                Path output = Path.of(args[2]);
-
-                CompressorUtil.filteringEvalGame(input, output);
-            }
-            case "filteringEmptyGameEvalGame" -> {
-                if (args.length != 3) {
-                    System.out.println("Usage: filteringEvalGame <input.zst> <output.zst>");
-                    return;
-                }
-
-                Path input = Path.of(args[1]);
-                Path output = Path.of(args[2]);
-
-                CompressorUtil.filteringEmptyGameEvalGame(input, output);
+                CompressorUtil.filter(input, output);
             }
             default -> System.out.println("Unknown command: " + args[0]);
         }
